7b0c9df89b92eae6adbe082ca5aa5922b247381e
hactuallybenji <mrbenamiller@gmail.com>
2023-09-28 18:48:58 -0700

    Input and adds first playground

--- going.go
+++ going.go
@@ -8,5 +8,18 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello, world!")
+  fmt.Println("Enter your first name: ");
+  
+  var firstName string;
+  fmt.Scanln(&firstName);
+
+  fmt.Println("Enter your last name: ");
+
+  var lastName string;
+  fmt.Scanln(&lastName);
+
+  fmt.Println("Your first name is " + firstName);
+  fmt.Println("Your last name is " + lastName);
+
+  fmt.Println("Welcome, " + firstName + " " + lastName);
 }
