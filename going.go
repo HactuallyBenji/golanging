6e1c1a3cbe03abc5481deb7562075d50a905fdba
hactuallybenji <mrbenamiller@gmail.com>
2023-09-28 19:28:52 -0700

    Adding type conversion

--- going.go
+++ going.go
@@ -5,8 +5,42 @@
 
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strconv"
+);
 
 func main() {
   fmt.Println("Hello, world!")
+  fmt.Println("Enter your first name: ");
+  
+  var firstName string;
+  fmt.Scanln(&firstName);
+
+  fmt.Println("Enter your last name: ");
+
+  var lastName string;
+  fmt.Scanln(&lastName);
+
+  fmt.Println("Your first name is " + firstName);
+  fmt.Println("Your last name is " + lastName);
+
+  fmt.Println("Welcome, " + firstName + " " + lastName);
+
+  fmt.Println("Enter an integer: ");
+
+  var firstNumber string;
+  fmt.Scanln(&firstNumber)
+
+  fmt.Println("Enter a second integer: ");
+
+  var secondNumber string;
+  fmt.Scanln(&secondNumber);
+
+  var firstInt int
+  var secondInt int
+
+  firstInt, _ = strconv.Atoi(firstNumber);
+  secondInt, _ = strconv.Atoi(secondNumber);
+  fmt.Println(firstInt + secondInt); 
 }
