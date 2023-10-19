3529ac3f2b7853c89531042c3f77a49eb91d57bb
hactuallybenji <mrbenamiller@gmail.com>
2023-10-18 19:07:13 -0700

    Implementation-specific (architecture) int data types

---
+++ implementation_specific_ints.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a uint = 20; // uint is an unsigned implementation-specific data type
+    fmt.Println("a uint:", a)
+
+    var b int = -30; //int is a signed implementation-specific data type
+    fmt.Println("b int:", b)
+}
