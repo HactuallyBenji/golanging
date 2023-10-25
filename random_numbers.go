a397249b412e17995cc7057cef44da50a0b82857
hactuallybenji <mrbenamiller@gmail.com>
2023-10-24 17:50:57 -0700

    Finding max value in an array is really useful

--- random_numbers.go
+++ random_numbers.go
@@ -2,10 +2,28 @@ package main
 
 import (
   "fmt"
+  "errors"
   "math/rand"
   "time"
   "strconv"
 )
+
+func findMax(array []int) (int, error) {
+
+  if len(array) == 0 {
+    return 0, errors.New("Empty array received. No max value exists") 
+  }
+
+  currentMax := array[0]
+
+  for i := 1; i < len(array); i++ {
+    if array[i] > currentMax {
+      currentMax = array[i]
+    }
+  }
+
+  return currentMax, nil
+}
 
 func main() {
   ns := rand.NewSource(time.Now().UnixNano())
@@ -25,4 +43,7 @@ func main() {
   }
 
   fmt.Println(randomArray)
+
+  maxValue, _ := findMax(randomArray)
+  fmt.Println(maxValue)
  }
