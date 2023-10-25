ed51834b58d628e5e8774f71363dc4b731f6d772
hactuallybenji <mrbenamiller@gmail.com>
2023-10-24 18:01:15 -0700

    Adding printed statements would become confusing without text to describe the statements

--- random_numbers.go
+++ random_numbers.go
@@ -25,6 +25,61 @@ func findMaxValue(array []int) (int, error) {
   return currentMax, nil
 }
 
+func findMaxIndex(array []int) (int, error) {
+  
+  if len(array) == 0 {
+    return 0, errors.New("Empty array received. No max value exists")
+  }
+
+  maxValueIndex := 0
+  currentMax := array[maxValueIndex]
+
+  for i := 0; i < len(array); i++ {
+    if array[i] > currentMax {
+      currentMax = array[i]
+      maxValueIndex = i
+    }
+  }
+
+  return maxValueIndex, nil
+}
+
+func findMinValue(array []int) (int, error) {
+
+  if len(array) == 0 {
+    return 0, errors.New("Empty array received. No min value exists") 
+  }
+
+  currentMin := array[0]
+
+  for i := 1; i < len(array); i++ {
+    if array[i] < currentMin {
+      currentMin = array[i]
+    }
+  }
+
+  return currentMin, nil
+}
+
+func findMinIndex(array []int) (int, error) {
+  
+  if len(array) == 0 {
+    return 0, errors.New("Empty array received. No min value exists")
+  }
+
+  minValueIndex := 0
+  currentMin := array[minValueIndex]
+
+  for i := 0; i < len(array); i++ {
+    if array[i] < currentMin {
+      currentMin = array[i]
+      minValueIndex = i
+    }
+  }
+
+  return minValueIndex, nil
+}
+
 func main() {
   ns := rand.NewSource(time.Now().UnixNano())
   generator := rand.New(ns)
@@ -44,6 +99,9 @@ func main() {
 
   fmt.Println(randomArray)
 
-  maxValue, _ := findMax(randomArray)
-  fmt.Println(maxValue)
+  maxValue, _ := findMaxValue(randomArray)
+  fmt.Println("The maximum value in the array is:", maxValue)
+
+  maxValueIndex, _ := findMaxIndex(randomArray)
+  fmt.Println("The maximum value's index in the array is:", maxValueIndex)
  }
