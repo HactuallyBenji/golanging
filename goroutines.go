7f1d83358bd4eb3033046076e0a7693147179c06
hactuallybenji <mrbenamiller@gmail.com>
2023-10-25 18:24:28 -0700

    Basic goroutines

---
+++ goroutines.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+   "fmt"
+)
+
+func goroutine() {
+   fmt.Println("This is my first goroutine.")
+}
+
+func main() {
+   go goroutine()
+
+   fmt.Println("main goroutine")
+}
