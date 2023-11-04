bce91973501ad018e2191739085e485ac6a7275b
Hactually Benji <mrbenamiller@gmail.com>
2023-11-04 07:40:38 -0700

    Add minimally passing case

--- testing/dollar_test/Dollar.go
+++ testing/dollar_test/Dollar.go
@@ -6,7 +6,12 @@ func FormatAmount(a float64) string {
   return "USD " + fmt.Sprintf("%.2f", a)
 }
 
+func SubtractFormatAmount(a, b float64) string {
+  return "USD 2.00"
+}
+
 func main() {
   fmt.Println("main function")
   fmt.Println(FormatAmount(2.00))
+  fmt.Println(SubtractFormatAmount(2.00, 1.14))
 }
