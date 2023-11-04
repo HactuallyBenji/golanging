6882340add2b3371516d33eada00413faf96aa8b
Hactually Benji <mrbenamiller@gmail.com>
2023-11-04 15:14:38 -0700

    Passing implementation

--- testing/dollar_test/Dollar.go
+++ testing/dollar_test/Dollar.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func FormatAmount(a float64) string {
+  if a < 0 {
+    return "Impossible operation"
+  }
   return "USD " + fmt.Sprintf("%.2f", a)
 }
 
 func SubtractFormatAmount(a, b float64) string {
+  if a < 0 || b < 0 {
+    return "Impossible operation"
+  }
   if a >= b {
     return "USD " + fmt.Sprintf("%.2f", (a - b))
   }
@@ -16,5 +22,9 @@ func SubtractFormatAmount(a, b float64) string {
 func main() {
   fmt.Println("main function")
   fmt.Println(FormatAmount(2.00))
+  fmt.Println(FormatAmount(-10.00))
+
   fmt.Println(SubtractFormatAmount(2.00, 1.14))
+  fmt.Println(SubtractFormatAmount(0.03 , 0.42))
+  fmt.Println(SubtractFormatAmount(-0.03 , -0.42))
 }
