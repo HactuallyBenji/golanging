0e6e7723bc45890cc01383f93cb4643a37eb6c3d
Hactually Benji <mrbenamiller@gmail.com>
2023-11-04 07:57:30 -0700

    Passing implementation

--- testing/dollar_test/Dollar.go
+++ testing/dollar_test/Dollar.go
@@ -7,7 +7,10 @@ func FormatAmount(a float64) string {
 }
 
 func SubtractFormatAmount(a, b float64) string {
-  return "USD " + fmt.Sprintf("%.2f", (a - b))
+  if a >= b {
+    return "USD " + fmt.Sprintf("%.2f", (a - b))
+  }
+  return "Impossible operation"
 }
 
 func main() {
